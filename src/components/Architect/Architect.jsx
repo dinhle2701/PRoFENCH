9167d75dfab4a0ed9bf9f4115dfcf4ae3bd92d4b
dinh-letat <dinhprococoa@gmail.com>
2026-05-02 12:39:42 +0700

    fix

--- src/components/Architect/Architect.jsx
+++ src/components/Architect/Architect.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { figure } from '@/data/figure'
 
 const Architect = () => {
+    const figureMap = Object.fromEntries(
+        figure.map(f => [f.id, f])
+    );
     return (
         <div className='methodology container-size mt-12' id='architect'>
             <h2 className='text-2xl text-black font-bold mt-12'>WiVi32-Fusion Architect</h2>
 
             <div className="overall-architecture">
-                <img src="/PRoFENCH/image/overall-architecture.png" alt="overall-architecture" className="mx-auto mt-8" />
+                <img
+                    src={figureMap[2]?.url}
+                    alt="overall-architecture"
+                    className="mx-auto mt-8" />
                 <p className='text-center'>
                     Figure 2: Overall architecture of the proposed WiVi32-Fusion framework.
                 </p>
